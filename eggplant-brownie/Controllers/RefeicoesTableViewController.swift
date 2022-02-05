553a2eb21aab1d3e603765eb29acf6966c8bccba
Juliano C L Santos <foxports@hotmail.com>
2022-02-05 00:39:25 -0300

    Programando tableView de selecao de ingredientes

---
+++ eggplant-brownie/Controllers/RefeicoesTableViewController.swift
@@ -0,0 +1,42 @@
+//
+//  RefeicaoViewController.swift
+//  eggplant-brownie
+//
+//  Created by Juliano Santos on 02/02/22.
+//
+
+import UIKit
+
+class RefeicoesTableViewController: UITableViewController, AdicionaRefeicaoDelegate {
+    
+    var refeicoes = [Refeicao(nome: "Churrasco", felicidade: 5),
+                     Refeicao(nome: "X-Tudo", felicidade: 3),
+                     Refeicao(nome: "Subway", felicidade: 4)]
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return refeicoes.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let celula = UITableViewCell(style: .default, reuseIdentifier: nil)
+        let refeicao = refeicoes[indexPath.row]
+        celula.textLabel?.text = refeicao.nome
+        
+        return celula
+    }
+    
+    func add(_ refeicao: Refeicao) {
+        refeicoes.append(refeicao)
+        tableView.reloadData()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "adicionar" {
+            if let viewController = segue.destination as? ViewController {
+                viewController.delegate = self
+            }
+        }
+    }
+    
+}
--- eggplant-brownie/Controllers/ViewController.swift
+++ eggplant-brownie/Controllers/ViewController.swift
@@ -7,22 +7,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+protocol AdicionaRefeicaoDelegate {
+    func add(_ refeicao: Refeicao)
+}
 
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+   
+    // MARK: - Attributes
+    
+    var delegate: AdicionaRefeicaoDelegate?
+    let ingredientes = ["Mulho Sugo", "Molho Quatro Queijos", "Molho Vermelho", "Tempero", "Oregano", "Pimenta Calabresa", "Ameixa Seca", "Tomate Seco", "Bacon", "Mussarela", "Azeitona", "Palmito", "Presunto", "Calabresa", "Queijo Ralado"]
+    
+    // MARK: - IBOutlet
+    
     @IBOutlet var nomeTextField: UITextField?
     @IBOutlet weak var felicidadeTextField: UITextField?
     
+    // MARK: - Actions
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return ingredientes.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celula = UITableViewCell(style: .default, reuseIdentifier: nil)
+        let ingrediente = ingredientes[indexPath.row]
+        celula.textLabel?.text = ingrediente
+        return celula
+    }
+    
+    // MARK: - UITableViewDelegate
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let celula = tableView.cellForRow(at: indexPath) else { return }
+        
+        if celula.accessoryType == .none {
+            celula.accessoryType = .checkmark
+        } else {
+            celula.accessoryType = .none
+        }
+            
+    }
+    
+    
     @IBAction func adicionar(_ sender: Any) {
 
-//        if let nomeDaRefeicao = nomeTextField?.text, let felicidadeDaRefeicao = felicidadeTextField?.text {
-//            let nome = nomeDaRefeicao
-//            if let felicidade = Int(felicidadeDaRefeicao) {
-//                print("Comi \(nome) e fiquei com felicidade \(felicidade)")
-//                let refeicao = Refeicao(nome: nome, felicidade: felicidade)
-//            } else {
-//                print("Erro ao criar refeicao")
-//            }
-//        }
+        /*if let nomeDaRefeicao = nomeTextField?.text, let felicidadeDaRefeicao = felicidadeTextField?.text {
+            let nome = nomeDaRefeicao
+            if let felicidade = Int(felicidadeDaRefeicao) {
+                print("Comi \(nome) e fiquei com felicidade \(felicidade)")
+                let refeicao = Refeicao(nome: nome, felicidade: felicidade)
+            } else {
+                print("Erro ao criar refeicao")
+            }
+        }*/
         
         guard let nome = nomeTextField?.text else {
             return
@@ -35,6 +72,10 @@ class ViewController: UIViewController {
 
         print("Comi \(nome) e fiquei com felicidade \(felicidade)")
         let refeicao = Refeicao(nome: nome, felicidade: felicidade)
+        
+        delegate?.add(refeicao)
+        
+        navigationController?.popViewController(animated: true)
     }
     
 }
